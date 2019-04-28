575964203ffa24bb4db315098dae966047b0a592
Tomas <amboraes@hotmail.com>
2019-04-28 13:13:46 -0500

    Se suben todos los archivos funcionales para databricks

--- Limpieza/limpieza.py
+++ Limpieza/limpieza.py
@@ -7,6 +7,7 @@ stop_words = stopwords.words('english')
 lines = spark.read.format("csv").option("header","true").option("mode", "DROPMALFORMED").load("dbfs:///FileStore/tables/all-the-news/*.csv")
 noticias = []
 cantidad = 0
+identRowless = []
 content = lines.select('content').collect()
 title = lines.select('title').collect()
 ident = lines.select('id').collect()
@@ -14,6 +15,7 @@ for x in range(len(content)):
   contentCln = findall(r"[\w']+", str(content[x][0]).lower())
   titleCln = findall(r"[\w']+", str(title[x][0]).lower())
   filtered_Content = [w for w in contentCln if not w in stop_words]
+  identRowless.append(ident[x][0])
   filtered_Title = [w for w in titleCln if not w in stop_words]
   aux = [ident[x][0],filtered_Title, filtered_Content]
   noticias.append(aux)
